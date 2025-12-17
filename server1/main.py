9980b4ab76471e9ae5c8498e5488d6d4b5309a40
Troppmop <troppmop@gmail.com>
2025-12-17 12:29:12 +0200

    routes set up

--- server1/main.py
+++ server1/main.py
@@ -1,14 +1,46 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import json
 
-app = FastAPI()
+DB_PATH = 'db/shopping_list.json'
 
-@app.get('items/')
+
+def load_database() -> dict:
+    """Load data from JSON file and return as dictionary"""
+    try:
+        with open(DB_PATH, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        with open(DB_PATH, 'w') as f:
+            json.dumps([])
+        
+        with open(DB_PATH, "r") as f:
+            return json.load(f)
+
+
+def save_database(data: dict) -> None:
+    """Save dictionary data to JSON file"""
+    with open(DB_PATH, "w") as f:
+        json.dump(data, f, indent=2)
+
+app = FastAPI(title="Items API", version="1.0.0")
+
+@app.get('/items/')
 def get_items():
-    pass
+    data = load_database()
+    return list(data)
 
-@app.post('items/')
-def add_item():
-    pass
+@app.post('/items/')
+def add_item(name: str, quantity: int):
+    data = load_database()
+    length = len(data)
+    item_dict = {'name': name, 'quantity': quantity}
+    item_dict.update({'id':length + 1})
+    data.append(item_dict)
+    save_database(data)
+    return {'message': f'item {item_dict['name']} added successfully'}
+    
+    
 
 if __name__ == "__main__":
     import uvicorn
